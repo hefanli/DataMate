536ef9f5563364ed9fb9c5e254d0059f143baaa3
hhhhsc701 <56435672+hhhhsc701@users.noreply.github.com>
2025-11-21 12:06:53 +0800

    feature: milvus service名称变更  兼容k8s (#97)
    feature: milvus service名称变更  兼容k8s (#97)

--- backend/services/rag-indexer-service/src/main/java/com/datamate/rag/indexer/infrastructure/milvus/MilvusService.java
+++ backend/services/rag-indexer-service/src/main/java/com/datamate/rag/indexer/infrastructure/milvus/MilvusService.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class MilvusService {
-    @Value("${datamate.rag.milvus-host:milvus-standalone}")
+    @Value("${datamate.rag.milvus-host:milvus}")
     private String milvusHost;
     @Value("${datamate.rag.milvus-port:19530}")
     private int milvusPort;
